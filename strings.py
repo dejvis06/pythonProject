b1d11047ae83c3885592577cfec0e7378fafb019
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-07 14:49:26 +0200

    try 'in' operator and add parameters by formating string

--- strings.py
+++ strings.py
@@ -18,3 +18,12 @@ print(parrot[0:6:3])
 
 number = "9,111,111,111,111"
 print(number[1::4])
+
+today = "friday"
+print("day" in today)
+print("not_in" in today)
+
+age = 24
+print("My age is: " + str(age))
+print("My age is: {0}".format(age))
+print("First param: {0}, second param: {1}".format(age, 25))
