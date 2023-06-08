b35260f5d0d727f5c4c137505128909db67e357d
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-08 13:51:28 +0200

    initialize variable to None

--- for-loop.py
+++ for-loop.py
@@ -16,3 +16,15 @@ for item in shopping_list:
         break
     else:
         print(item)
+
+print("-----------")
+
+found_at = None
+item_to_find = "pasta1"
+
+for index in range(len(shopping_list)):
+    if shopping_list[index] == item_to_find:
+        found_at = index
+        break
+
+print("Found at index {}".format(found_at))
