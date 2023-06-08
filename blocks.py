a865d379a8d8b673d3eb5bda234a99aea282b59f
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-08 12:12:10 +0200

    use OR in condition

--- blocks.py
+++ blocks.py
@@ -5,6 +5,8 @@ print(age)
 
 if age == 900:
     print("Sorry, Yoda, you die in Return of the Jedi")
+elif age == 901 or age == 902:
+    print("Sorry, Yoda, you are still dead")
 elif age >= 18:
     print("You are old enough to vote")
 else:
