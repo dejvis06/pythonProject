cb8ec473a25213448c6262c83b53cbe77304f69c
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-08 13:56:54 +0200

    add while loops

---
+++ while-loop.py
@@ -0,0 +1,14 @@
+i = 0;
+while i < 10:
+    print("i is: {}".format(i))
+    i += 1
+
+print("--------------")
+
+i = 0;
+while True:
+    i += 1
+    if i == 5:
+        break
+
+print("Value of i after break: {}".format(i))
