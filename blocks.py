b5dfead80780b3dac3b70785408afaa351e6c2b0
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-08 11:59:36 +0200

    add if/else block

---
+++ blocks.py
@@ -0,0 +1,9 @@
+name = input("Please enter your name: ")
+age = int(input("Please enter your age {0}: ".format(name)))
+
+print(age)
+
+if age >= 18:
+    print("You are old enough to vote")
+else:
+    print("Please come back in {0} years".format(18 - age))
