aeba7500b9b26e354da7b17dd8cf5045ade29fb8
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-08 12:03:16 +0200

    add elif statement

--- blocks.py
+++ blocks.py
@@ -3,7 +3,14 @@ age = int(input("Please enter your age {0}: ".format(name)))
 
 print(age)
 
-if age >= 18:
+if age == 900:
+    print("Sorry, Yoda, you die in Return of the Jedi")
+elif age >= 18:
     print("You are old enough to vote")
 else:
     print("Please come back in {0} years".format(18 - age))
+
+
+
+
+
