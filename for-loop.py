0e50f14ecac71018fca49a4b536f45f3ee1de089
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-08 13:42:38 +0200

    add break statement inside loop

--- for-loop.py
+++ for-loop.py
@@ -2,9 +2,17 @@ parrot = "Norwegian Blue"
 for character in parrot:
     print(character)
 
-shopping_list = ["milk", "pasta", "eggs", "spam","bread"]
+shopping_list = ["milk", "pasta", "eggs", "spam", "bread"]
 for item in shopping_list:
     if item == "spam":
         continue
+    else:
+        print(item)
+
+print("-----------")
+
+for item in shopping_list:
+    if item == "spam":
+        break
     else:
         print(item)
