297adf81e04fd338d7430f867ff9ed522e17c553
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-08 13:42:08 +0200

    add continue statement inside loop

---
+++ for-loop.py
@@ -0,0 +1,10 @@
+parrot = "Norwegian Blue"
+for character in parrot:
+    print(character)
+
+shopping_list = ["milk", "pasta", "eggs", "spam","bread"]
+for item in shopping_list:
+    if item == "spam":
+        continue
+    else:
+        print(item)
