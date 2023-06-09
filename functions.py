294302e1eeb0c87671d66b35e45b747ecd17b179
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-09 13:39:07 +0200

    raise and handle exception

--- functions.py
+++ functions.py
@@ -12,3 +12,14 @@ def multiply(x, y):
 
 
 print(multiply(10.5, 4))
+
+
+def add(x, y):
+    try:
+        if x < 5:
+            raise ValueError("X cannot be lower than 5")
+        return x + y
+    except ValueError:
+        return None
+
+print(add(3, 5))
