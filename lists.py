c066ef3e5c34ec9e4d13e0b32d0a5a371f91dc60
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-09 11:05:31 +0200

    sort lists

--- lists.py
+++ lists.py
@@ -17,3 +17,21 @@ for index, part in enumerate(computer_parts):
 
 computer_parts.remove("another part")
 print(computer_parts)
+
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+even.extend(odd)
+print(even)
+
+even.sort()
+print(even)
+
+even.sort(reverse=True)
+print(even)
+
+computer_parts.sort()
+print(computer_parts)
+
+computer_parts.sort(reverse=True)
+print(computer_parts)
