c642d78b2aaa4dfe75f966136a7f28363a4d73bf
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-12 10:48:30 +0200

    loop, add update and delete element

--- dictionaries.py
+++ dictionaries.py
@@ -10,3 +10,27 @@ print(dream_car)
 
 roadster = vehicles.get('roadster')
 print(roadster)
+
+print('--------')
+
+for key in vehicles:
+    print(key)
+
+for key in vehicles:
+    print(key, vehicles[key])
+
+for key, value in vehicles.items():
+    print(key, value, sep=', ')
+
+print('--------------')
+
+vehicles['starfighter'] = "Lockheed F-104"
+print(vehicles['starfighter'])
+vehicles['starfighter'] = "Lockheed F-1041"
+print(vehicles['starfighter'])
+
+vehicles.__delitem__('starfighter')
+print(vehicles)
+
+del vehicles['dream']
+print(vehicles)
