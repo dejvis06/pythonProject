8bfe89c142347b7b438f9e2344c8be7460f89057
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-13 14:00:55 +0200

    create class, init object and print values

---
+++ object_oriented.py
@@ -0,0 +1,16 @@
+class Kettle(object):
+
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+
+kenwood = Kettle("Kenwood", 8.99)
+print(kenwood.make)
+print(kenwood.price)
+
+print()
+
+kenwood.price = 12.75
+print(kenwood.price)
